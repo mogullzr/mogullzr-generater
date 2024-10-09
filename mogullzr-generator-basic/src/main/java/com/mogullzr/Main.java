e7e097524288fbbe7162fb4fcb5d72c4580fea42
mogullzr <898561494@qq.com>
2024-10-09 14:38:23 +0800

    1、第一次提交

--- mogullzr-generator-basic/src/main/java/com/mogullzr/Main.java
+++ mogullzr-generator-basic/src/main/java/com/mogullzr/Main.java
@@ -4,6 +4,7 @@ package com.mogullzr;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("1231");
+        System.out.println("1231::q" +
+                "");
     }
 }
